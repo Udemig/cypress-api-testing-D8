1e0448021a7f638a268e82c7bb5b2768907a310a
can-seker <can.sekerr@outlook.com>
2023-05-08 00:02:24 +0300

    Add project
    Add project

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "9693yt",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
